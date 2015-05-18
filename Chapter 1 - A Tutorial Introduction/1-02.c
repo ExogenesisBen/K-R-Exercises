7519a68ec2c98ab63729211e3c13d2fc1744fbcb
Ben Jackson <benatkilmory@gmail.com>
2015-05-18 17:15:13 +0100

    Exercise 1-02 complete

---
+++ Chapter 1 - A Tutorial Introduction/1-02.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+main()
+{
+	printf("\n--\n");
+	/* Normal */
+	printf("hello, world");
+	
+	printf("\n--\n");
+	/* Tab */
+	printf("hello, \t world");
+	
+	printf("\n--\n");
+	/* Backspace */
+	printf("hello, \b\b\b world");
+	
+	printf("\n--\n");
+	/* Alert */
+	printf("hello, world\a");
+	
+	printf("\n--\n");
+}
